4ccfdcaff4c6cf9fbf6a18aa482f4a4a4dde94e5
Sergio Gaino <eng.gaino@gmail.com>
2018-05-25 00:58:51 -0300

    filter by regular expression added

--- src/pipes/search/search.ts
+++ src/pipes/search/search.ts
@@ -15,9 +15,20 @@ export class SearchPipe implements PipeTransform {
   transform(items: any[], terms: string): any[] {
     if (!items) return [];
     if (!terms) return items;
+
+    let regexString = '';
+
+    for (let i = 0; i < terms.length; i++) {
+      regexString += terms.charAt(i) + '.*';
+    }
+    
+    var rx = new RegExp(regexString, 'i');
+ 
     terms = terms.toLowerCase();
     return items.filter(it => {
-      return it.title.toLowerCase().includes(terms); // only filter notes title
+      return it.title.toLowerCase().search(rx) != -1; // only filter notes title
     });
+
+    
   }
 }
